716d29aa0ac320bd2ddcec107f8d241375fcbba6
Suraj Nag <suraj11nag@gmail.com>
2023-06-11 11:07:18 +0530

    streamlit

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pickle
+
+# Load the pickled model
+with open('spam_classifier_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Load the pickled vectorizer
+with open('vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
+
+# Create the Streamlit user interface
+st.title("Spam Filter")
+
+message = st.text_area("Enter a message")
+if st.button("Predict"):
+    # Preprocess the input message
+    preprocessed_message = preprocess_message(message)
+
+    # Vectorize the preprocessed message
+    vectorized_message = vectorizer.transform([preprocessed_message])
+
+    # Make the prediction
+    prediction = model.predict(vectorized_message)[0]
+
+    # Display the prediction
+    if prediction == 'ham':
+        st.success("Not spam")
+    else:
+        st.error("Spam")
